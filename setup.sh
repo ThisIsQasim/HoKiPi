1e2e81335e075c8246e25b1f0c7c11c5f0fbe03e
ThisIsQasim <QasRoX@hotmail.com>
2016-12-06 00:27:07 +0500

    remove sudo from commands

--- setup.sh
+++ setup.sh
@@ -8,13 +8,13 @@ wget https://nodejs.org/dist/v6.9.1/node-v6.9.1-linux-armv6l.tar.xz
 
 tar -xvf node-v6.9.1-linux-armv6l.tar.xz
 
-sudo cp -R node-v6.9.1-linux-armv6l/* /usr/local/
+cp -R node-v6.9.1-linux-armv6l/* /usr/local/
 
-sudo npm install -g --unsafe-perm homebridge hap-nodejs node-gyp
+npm install -g --unsafe-perm homebridge hap-nodejs node-gyp
 cd /usr/local/lib/node_modules/homebridge/
-sudo npm install --unsafe-perm bignum
+npm install --unsafe-perm bignum
 cd /usr/local/lib/node_modules/hap-nodejs/node_modules/mdns
-sudo node-gyp BUILDTYPE=Release rebuild
+node-gyp BUILDTYPE=Release rebuild
 
 npm install -g forever
 npm install -g forever-service
