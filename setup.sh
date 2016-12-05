2f20b958288ad8f2816169eee1d0b84fe098b220
ThisIsQasim <QasRoX@hotmail.com>
2016-12-06 02:25:09 +0500

    Set pin modes to output on boot

--- setup.sh
+++ setup.sh
@@ -123,6 +123,18 @@ KillMode=process
 WantedBy=multi-user.target
 EOF'
 
+sudo bash -c 'cat <<EOF >> /etc/rc.local
+gpio export $pin1 out
+gpio export $pin2 out
+gpio export $pin3 out
+gpio export $pin4 out
+EOF'
+
+sudo gpio export $pin1 out
+sudo gpio export $pin2 out
+sudo gpio export $pin3 out
+sudo gpio export $pin4 out
+
 sudo systemctl deamon-reload
 sudo systemctl start homebridge
 sudo systemctl enable homebridge
